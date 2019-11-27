df7127f0bc1d6e9fb1fbbf55abb06cb0da9fe9b0
hzh <2603894867@qq.com>
2019-11-27 20:30:13 +0800

    第三次提交：除掉注解

--- ch02-spring-ioc/src/main/java/complex/UserDaoImpl.java
+++ ch02-spring-ioc/src/main/java/complex/UserDaoImpl.java
@@ -2,8 +2,9 @@ package complex;
 
 public class UserDaoImpl implements UserDao{
 
+
     public void update(){
-        //输出update----
+
         System.out.println("update in userdao...");
     }
 }
