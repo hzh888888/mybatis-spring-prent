3268b6fe0f753058b633a3c2c826002b8322e764
hzh888888 <58217411+hzh888888@users.noreply.github.com>
2019-11-27 20:13:45 +0800

    hzh
    加了main方法

--- ch02-spring-ioc/src/main/java/complex/Main.java
+++ ch02-spring-ioc/src/main/java/complex/Main.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
+    //Main方法
 
     public static void main(String[] args) {
         ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext_complex.xml");
